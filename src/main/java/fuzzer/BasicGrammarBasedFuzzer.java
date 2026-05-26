34e2920bc9412ff839b9263adee8d07d0017ef6d
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2026-05-26 10:12:52 +0200

    Change indent size

--- src/main/java/fuzzer/BasicGrammarBasedFuzzer.java
+++ src/main/java/fuzzer/BasicGrammarBasedFuzzer.java
@@ -8,66 +8,66 @@ import java.util.regex.Pattern;
 
 public class BasicGrammarBasedFuzzer extends GrammarBasedFuzzer {
 
-  private static final int MAX_NONTERMINALS = 6;
-  private static final int MAX_EXPANSION_TRIALS = 100;
-  private static final String RE_NONTERMINAL = "(<[^<> ]*>)";
+    private static final int MAX_NONTERMINALS = 6;
+    private static final int MAX_EXPANSION_TRIALS = 100;
+    private static final String RE_NONTERMINAL = "(<[^<> ]*>)";
 
-  protected Random rand;
+    protected Random rand;
 
-  public BasicGrammarBasedFuzzer(FuzzerConfig config) {
-    grammar = config.getGrammar();
-    rand = new Random(config.getSeed());
-  }
-
-  /**
-   * Fuzz the grammar and returns a valid expression
-   */
-  @SuppressWarnings("unchecked")
-  public String generate() {
-    String term = INITIAL_SYMBOL;
-    int expansion_trials = 0;
-    List<String> non_terminals = nonTerminals(term);
-    while (!non_terminals.isEmpty()) {
-      String symbol_to_expand = non_terminals.get(rand.nextInt(non_terminals.size()));
-      List<Object> expansions = (List<Object>) grammar.get(symbol_to_expand);
-      String expansion = (String) expansions.get(rand.nextInt(expansions.size()));
-      String new_term = term.replaceFirst(symbol_to_expand, expansion);
-      non_terminals = nonTerminals(new_term);
-      if (non_terminals.size() < MAX_NONTERMINALS) {
-        term = new_term;
-        expansion_trials = 0;
-      } else {
-        expansion_trials += 1;
-        if (expansion_trials >= MAX_EXPANSION_TRIALS)
-          throw new IllegalStateException("Can't expand " + term);
-      }
+    public BasicGrammarBasedFuzzer(FuzzerConfig config) {
+        grammar = config.getGrammar();
+        rand = new Random(config.getSeed());
     }
-    return term;
-  }
 
-
-  /**
-   * Fuzz the grammar the given number of times
-   */
-  public List<String> generate(int n) {
-    if (n <= 0) throw new IllegalArgumentException("The amount of expressions to fuzz must be a positive number");
-    List<String> fuzzed = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      fuzzed.add(generate());
+    /**
+     * Fuzz the grammar and returns a valid expression
+     */
+    @SuppressWarnings("unchecked")
+    public String generate() {
+        String term = INITIAL_SYMBOL;
+        int expansion_trials = 0;
+        List<String> non_terminals = nonTerminals(term);
+        while (!non_terminals.isEmpty()) {
+            String symbol_to_expand = non_terminals.get(rand.nextInt(non_terminals.size()));
+            List<Object> expansions = (List<Object>) grammar.get(symbol_to_expand);
+            String expansion = (String) expansions.get(rand.nextInt(expansions.size()));
+            String new_term = term.replaceFirst(symbol_to_expand, expansion);
+            non_terminals = nonTerminals(new_term);
+            if (non_terminals.size() < MAX_NONTERMINALS) {
+                term = new_term;
+                expansion_trials = 0;
+            } else {
+                expansion_trials += 1;
+                if (expansion_trials >= MAX_EXPANSION_TRIALS)
+                    throw new IllegalStateException("Can't expand " + term);
+            }
+        }
+        return term;
     }
-    return fuzzed;
-  }
 
-  /**
-   * Get the amount of non-terminals symbols of the given symbol
-   */
-  protected List<String> nonTerminals(String symbol) {
-    List<String> matches = new LinkedList<String>();
-    Matcher m = Pattern.compile("(?=(" + RE_NONTERMINAL + "))").matcher(symbol);
-    while (m.find()) {
-      matches.add(m.group(1));
+
+    /**
+     * Fuzz the grammar the given number of times
+     */
+    public List<String> generate(int n) {
+        if (n <= 0) throw new IllegalArgumentException("The amount of expressions to fuzz must be a positive number");
+        List<String> fuzzed = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            fuzzed.add(generate());
+        }
+        return fuzzed;
     }
-    return matches;
-  }
+
+    /**
+     * Get the amount of non-terminals symbols of the given symbol
+     */
+    protected List<String> nonTerminals(String symbol) {
+        List<String> matches = new LinkedList<String>();
+        Matcher m = Pattern.compile("(?=(" + RE_NONTERMINAL + "))").matcher(symbol);
+        while (m.find()) {
+            matches.add(m.group(1));
+        }
+        return matches;
+    }
 
 }
--- src/main/java/fuzzer/GrammarBasedFuzzer.java
+++ src/main/java/fuzzer/GrammarBasedFuzzer.java
@@ -6,21 +6,22 @@ import java.util.List;
 
 public abstract class GrammarBasedFuzzer {
 
-  protected JSONObject grammar;
+    protected JSONObject grammar;
 
-  protected String INITIAL_SYMBOL = "<S>";
+    protected String INITIAL_SYMBOL = "<S>";
 
-  public void setInitialSymbol(String initialSymbol) {
-    INITIAL_SYMBOL = initialSymbol;
-  }
+    public void setInitialSymbol(String initialSymbol) {
+        INITIAL_SYMBOL = initialSymbol;
+    }
 
-  /**
-   * Returns an expression from the grammar.
-   */
-  public abstract String generate();
+    /**
+     * Returns an expression from the grammar.
+     */
+    public abstract String generate();
 
-  /**
-   * Returns a list of fuzzed expressions from the grammar
-   */
-  public abstract List<String> generate(int n);
+    /**
+     * Returns a list of fuzzed expressions from the grammar
+     */
+    public abstract List<String> generate(int n);
+
 }
